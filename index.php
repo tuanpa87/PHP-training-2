35a64d0f5a3e08c3694f4f39b4e2921a72374bfc
tuanpa87 <tuanpa87@users.noreply.github.com>
2019-09-16 23:33:24 +0700

    html

--- index.php
+++ index.php
@@ -1,5 +1,33 @@
 <?php //echo "hello world"; ?>
 <?php 
-$name = "John Doe";
-echo "Hello, $name";
-?>
+// $name = "John Doe";
+// echo "Hello, $name";
+// ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <style>
+    header {
+      padding: 2rem;
+      background-color: #e3e3e3;
+      text-align: center
+    }
+  </style>
+</head>
+<body>
+  <header>
+    <h1>
+      <?php
+        $name = $_GET['name'];
+        echo "Hello, $name"
+      ?>
+    </h1>
+  </header>
+   
+</body>
+</html>
