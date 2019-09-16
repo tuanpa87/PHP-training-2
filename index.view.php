ff1a6b42da192edc8ea9d7e2d45f6e8494b2c60d
tuanpa87 <tuanpa87@users.noreply.github.com>
2019-09-16 23:46:29 +0700

    php and html

---
+++ index.view.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <style>
+    header {
+      padding: 2rem;
+      background-color: #e3e3e3;
+      text-align: center
+    }
+  </style>
+</head>
+<body>
+  <header>
+    <h1>
+      <?= $greeting; ?>
+    </h1>
+  </header>
+   
+</body>
+</html>
